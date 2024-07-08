ddcda9d6903ea7220211bbb64ca745c4542c5ca5
iris-ayelin <ayelin.iris@gmail.com>
2024-07-08 22:09:58 +0300

    add routing for 3 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HelpComponent } from './help/help.component';
+import { ChooseGameComponent } from './choose-game/choose-game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, FooterComponent, HeaderComponent, DashboardComponent, HelpComponent, ChooseGameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HelpComponent } from './help/help.component';
+import { ChooseGameComponent } from './choose-game/choose-game.component';
 
 export const routes: Routes = [
     {path: "", component: CategoriesListComponent},
     {path: "category/:id", component: CategoryFormComponent},
     {path: "newcategory", component: CategoryFormComponent},
+    {path: "dashboard", component: DashboardComponent},
+    {path: "help", component: HelpComponent},
+    {path: "choose-game", component: ChooseGameComponent}
 ];

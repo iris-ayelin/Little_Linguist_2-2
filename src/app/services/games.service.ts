af5830798002bd1dcd289e6fbd9c338141976be1
edenfalaka <edenfalaka@gmail.com>
2024-07-16 22:58:37 +0300

    Create Choose game page

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { GameProfile } from '../../shared/model/games';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
   
-  private games : GameProfile[] = [new GameProfile(), new GameProfile()]
+  private games : GameProfile[] = [new GameProfile()]
 
+ 
   public get() : GameProfile[] {
     return this.games;
   }
+  
 }
--- src/shared/model/games.ts
+++ src/shared/model/games.ts
@@ -1,9 +1,23 @@
 export class GameProfile {
 
-    constructor(
-        public gameId: string,
-        public gameName : string,
-        public gameDiscription : string,
-        public gameUrl : string) {
-    }
+    MixedWordsGame = [
+        '1', //gameid
+        'Mixed Words Game', // name
+        'Create a word from the preview mixed letters', // description
+        '/first-game', // URL
+    ]
+
+    MatchWordsGame = [
+        '2', //gameid
+        'Match Words Game', // name
+        'Compare and match between the preview words', // description
+        '/second-game', // URL
+    ]
+
+    // constructor(
+    //     public gameId: string,
+    //     public gameName : string,
+    //     public gameDiscription : string,
+    //     public gameUrl : string) {
+    // }
 }

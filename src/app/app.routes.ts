193cf2ad1d3cdbfa9d7394b3f2293158ea632a89
iris-ayelin <ayelin.iris@gmail.com>
2024-07-27 00:29:24 +0300

    more changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HelpComponent } from './help/help.component';
 import { ChooseGameComponent } from './choose-game/choose-game.component';
 import { MatchWordsComponent } from './match-words-game/match-words-game.component';
-import { MixedLettersGameComponent } from './mixed-letters-game/mixed-letters-game.component'; 
+import { MixedLettersGameComponent } from './mixed-letters/mixed-letters-game.component';
 import { WordSorterGameComponent } from './word-sorter-game/word-sorter-game.component';
 
 export const routes: Routes = [
-    {path: "", component: CategoriesListComponent},
+    {path: "", component: DashboardComponent},
     {path: "admin", component:CategoriesListComponent},
     {path: "category/:id", component: CategoryFormComponent},
     {path: "newcategory", component: CategoryFormComponent},

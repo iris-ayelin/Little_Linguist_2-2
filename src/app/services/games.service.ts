f78a5db1de3dd3d7a958e561a6733cdf7d02aa01
iris-ayelin <ayelin.iris@gmail.com>
2024-08-03 16:47:16 +0300

    Update game service file

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -8,20 +8,20 @@ export class GamesService {
   private games: GameProfile[] = [
     new GameProfile(
       '1',
-      'Mixed Letters',
+      'Mixed letters',
       'reate a word from the preview mixed letters',
       '/mixed-letters-game'
     ),
     new GameProfile(
       '2',
-      'Match Words Game',
+      'Match words',
       'Compare and match between the preview words',
       '/match-words-game'
     ),
 
     new GameProfile(
       '3',
-      'Sorted Words Game',
+      'Sorted words',
       'Arrange the jumbled words in the correct order to make',
       '/sorted-words-game'
     ),

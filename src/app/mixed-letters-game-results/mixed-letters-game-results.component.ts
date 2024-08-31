d8a0198fbdf53ab3a47983695715c737895e896a
edenfalaka <edenfalaka@gmail.com>
2024-09-01 01:31:17 +0300

    Result Page: Fix play again button + edit design

--- src/app/mixed-letters-game-results/mixed-letters-game-results.component.ts
+++ src/app/mixed-letters-game-results/mixed-letters-game-results.component.ts
@@ -3,6 +3,8 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { GamesService } from '../services/games.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 interface WordResult {
   hebrewWord: string;
@@ -13,7 +15,7 @@ interface WordResult {
 @Component({
   selector: 'app-mixed-letters-game-results',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
   templateUrl: './mixed-letters-game-results.component.html',
   styleUrls: ['./mixed-letters-game-results.component.css']
 })
@@ -23,13 +25,20 @@ export class MixedLettersGameResultsComponent implements OnInit {
   coins: number = 0;
   displayedColumns: string[] = ['hebrewWord', 'guessedWord', 'status'];
   dataSource: WordResult[] = [];
-
-  constructor(private gamesService: GamesService) {} 
+  
+  constructor(
+    private gamesService: GamesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.correctCount = this.gamesService.getCorrectAnswers();
     this.incorrectCount = this.gamesService.getIncorrectAnswers();
     this.coins = this.gamesService.getCoins();
     this.dataSource = this.gamesService.getResults();
+  }
+
+  play_again() {
+    this.router.navigate(["/lets-play"]);
   }
 }

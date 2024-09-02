b82410e447358b8e803a6ba475859220a5bf0c12
edenfalaka <edenfalaka@gmail.com>
2024-09-02 19:53:43 +0300

    match-word-game: add arrays for play, edit html, edit css

--- src/app/matching-game-module/check-word-dialog/check-word-dialog.component.ts
+++ src/app/matching-game-module/check-word-dialog/check-word-dialog.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-check-word-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatDialogActions, MatDialogClose, MatDialogContent],
   templateUrl: './check-word-dialog.component.html',
   styleUrl: './check-word-dialog.component.css'
 })
 export class CheckWordDialogComponent {
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { isCorrect: boolean }) {}
 }

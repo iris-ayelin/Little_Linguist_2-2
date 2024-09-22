f867162bff32789e25e6f86c921acf686f22af04
iris-ayelin <ayelin.iris@gmail.com>
2024-09-22 21:00:42 +0300

    Convert the generated ID from FS to sorted numbers

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../../shared/model/category';
-import { addDoc, collection, DocumentSnapshot, Firestore, getDocs, QuerySnapshot } from '@angular/fire/firestore';
+import {
+  addDoc,
+  collection,
+  doc,
+  DocumentSnapshot,
+  Firestore,
+  getDocs,
+  QuerySnapshot,
+  setDoc,
+} from '@angular/fire/firestore';
 import { categoryConverter } from './converter/category-converter';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class CategoriesService {
   constructor(private firestore: Firestore) {}
 
@@ -28,23 +36,33 @@ export class CategoriesService {
         result.push(data);
       }
     });
-    console.log('Raw Firestore Data:', result)
     return result;
   }
-  
 
   get(id: string): Category | undefined {
     return undefined;
   }
 
-  async add(newCategtoryData: Category) {
-    await addDoc(
-      collection(this.firestore, 'categories').withConverter(categoryConverter),
-      newCategtoryData
-    );
+  async add(newCategoryData: Category) {
+    const categoriesRef = collection(
+      this.firestore,
+      'categories'
+    ).withConverter(categoryConverter);
+
+    const querySnapshot = await getDocs(categoriesRef);
+    let maxId = 0;
+
+    querySnapshot.forEach((doc) => {
+      const docId = parseInt(doc.id, 10);
+      if (docId > maxId) {
+        maxId = docId;
+      }
+    });
+    const newId = (maxId + 1).toString();
+    await setDoc(doc(categoriesRef, newId), newCategoryData);
   }
 
-  update(existingPerson: Category): void {}
+  update(existingCategory: Category): void {}
 
-  delete(existingPersonId: string): void {}
+  delete(existingCategoryId: string): void {}
 }
